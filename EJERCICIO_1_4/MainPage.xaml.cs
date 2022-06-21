2e10e6dd5dcd5ffae0c68dcd04e40ed3063c0f1b
BrayanUTH <10brayanalvarez@gmail.com>
2022-06-21 11:28:55 -0600

    Agregar archivos de proyecto.

---
+++ EJERCICIO_1_4/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using EJERCICIO_1_4.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EJERCICIO_1_4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnViewList_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ListPage());
+        }
+
+        private void btnViewRegister_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+    }
+}
---
+++ EJERCICIO_1_4/Views/ListPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using EJERCICIO_1_4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EJERCICIO_1_4.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListPage : ContentPage
+    {
+        Employee selectedEmployee;
+        public ListPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            if (await App.DBase.GetAllEmployee() != null)
+            {
+                listEmple.ItemsSource = await App.DBase.GetAllEmployee();
+            }
+            else
+            {
+                await DisplayAlert("Advertencia", "No hay empleados agregados", "Ok");
+            }
+        }
+
+        private async void listEmple_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+                selectedEmployee = (e.CurrentSelection.FirstOrDefault() as Employee);
+                
+        }
+
+        private async void BtnViewEmployees_Clicked(object sender, EventArgs e)
+        {
+            bool result = await DisplayAlert("Confirmacion", "¿Esta seguro de visualiar el empleado?", "Si", "No");
+            if (result)
+            {
+                ViewPhotoPage photoPage = new ViewPhotoPage();
+                photoPage.BindingContext = selectedEmployee;
+                await Navigation.PushAsync(photoPage);
+            }
+        }
+    }
+}
